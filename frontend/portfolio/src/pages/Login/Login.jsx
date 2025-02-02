cd77be8c522ea0aff2367d56aa761a16f5318c1d
PunamDahal <dahalpunam68@gmail.com>
2025-02-02 22:22:09 +0545

    home and login created

--- frontend/portfolio/src/pages/Login/Login.jsx
+++ frontend/portfolio/src/pages/Login/Login.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import Button from "../../components/Button/Button";
+import styles from "./Login.module.css";
+import Header from '../../components/Header/Header'
+import Footer from "../../components/Footer/Footer";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div>
+    <Header />
+    <div className={styles.loginContainer}>
+      <div className={styles.loginBox}>
+        <h1>Sign in to CreateIT</h1>
+        <p>Login or register to start building your portfolio today.</p>
+        <Button text="Sign in with Google"  />
+        <Button text="Sign in with GitHub"  />
+        <Button text="Sign in with LinkedIn"  />
+      </div>
+    </div>
+    <Footer />
+    </div>
+  );
+};
 
-export default Login
+export default Login;
