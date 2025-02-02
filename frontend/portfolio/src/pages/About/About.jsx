32401e7eab1283cb0b4de8bb368535fffb6849c1
PunamDahal <dahalpunam68@gmail.com>
2025-02-02 22:39:37 +0545

    about and components

---
+++ frontend/portfolio/src/pages/About/About.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+
+      <main>
+       <section>
+        </section>
+       
+      </main>
+
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
